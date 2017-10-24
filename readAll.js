ff907aeda46299862277a5c1b1e92203ee31232e
zemla4ok <dima-vital@live.com>
2017-10-24 12:31:59 +0300

    sorting articles by id

--- readAll.js
+++ readAll.js
@@ -1,8 +1,30 @@
 const log = require('./log');
 const file = require('fs').createWriteStream('logfile.log');
+const ascOrder = 'asc';
+const descOrder = 'desc';
 let articles = require('./articles.json');
+let sortedArticles;
 
 module.exports.readAll = function readAll(req, res, payload, cb) {
+    sortedArticles = articles;
+    console.log(payload.sortField);
+    switch(payload.sortField){
+        case 'id':
+            console.log('hey');
+            sortOrder(payload, (a, b)=>{
+                return a.id - b.id;
+            })
+            break;
+    }
+   
+   
     log.log(file, '/api/articles/readall', payload);
-    cb(null, articles);
+    cb(null, sortedArticles);
+}
+
+function sortOrder(payload, func){
+    sortedArticles.sort(func);
+    if(payload.sortOrder === ascOrder){
+        sortedArticles.reverse();
+    }
 }
